bd68d58ac542d16fc7c0bafedae7acd59f3a1e2a
Randolph Burgos <randolphburgos@yahoo.com>
2020-05-25 12:23:27 +0800

    Added functions to return theme URL and images URL.

--- BM/functions/bm-functions.php
+++ BM/functions/bm-functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function bm_theme_url() {
+	return get_template_directory_uri();
+}
+
+function bm_images_url($filespec) {
+	return bm_theme_url() . '/assets/images/' . $filespec;
+}
+
 function format_phone_number($number) {
 	return preg_replace('/[^0-9]/', '', $number);
 }
